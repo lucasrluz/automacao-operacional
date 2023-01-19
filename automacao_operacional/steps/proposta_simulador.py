350bae3ffff3363d7bb725315865f4292319dc3a
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-19 14:28:47 -0300

    adicionado simulador de propostas

---
+++ automacao_operacional/steps/proposta_simulador.py
@@ -0,0 +1,86 @@
+from playwright.sync_api import Page
+from steps.util.data import Data
+from .util.element_identifiers import (
+    PROPOSTA_SIMULADOR,
+    DADOS_DA_PROPOSTA_PRODUTO,
+    DADOS_DA_PROPOSTA_TIPO_DE_OPERACAO,
+    DADOS_DA_PROPOSTA_ORGAO_OU_EMPREGADOR,
+    DADOS_DA_PROPOSTA_BANCO,
+    DADOS_DO_CLIENTE_CPF,
+    DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO,
+    DADOS_DO_CLIENTE_NOME_DO_CLIENTE,
+    DADOS_DO_REPRESENTANTE_LEGAL_CPF,
+    DADOS_DO_REPRESENTANTE_LEGAL_NOME
+)
+
+from time import sleep
+
+tipo_de_operacao = {
+    'AVERBAÇÃO ANTECIPADA DIGITAL':             '31',
+    'CARTÃO CONSIGNADO BENEFÍCIO':              '33',
+    'CARTÃO CONSIGNADO BENEFÍCIO (REPR LEGAL)': '36',
+    'CARTÃO DIGITAL RMC':                       '11',
+    'CARTÃO DIGITAL RMC (REPR LEGAL)':          '38',
+    'NOVO DIGITAL':                             '13',
+    'NOVO DIGITAL (REPR LEGAL)':                '35',
+    'PORTABILIDADE DIGITAL':                    '17',
+    'PORTABILIDADE MANUAL DIGITAL':             '28',
+    'REFIN CARTÃO DIGITAL':                     '29',
+    'REFIN DA PORTABILIDADE DIGITAL':           '18',
+    'REFIN DIGITAL':                            '14',
+    'REFIN DIGITAL + MARGEM LIVRE':             '32',
+    'REFIN RETENCAO DIGITAL':                   '15',
+    'PORTABILIDADE + REFIN DA PORTABILIDADE':   '003500'
+}
+
+orgao_ou_empregador = {
+    'GOVERNO DO ESTADO RS':             '1',
+    'INSS':                             '3',
+    'SIAPE':                            '15',
+    'FACTA FÁCIL - DÉBITO EM CONTA ':   '390',
+    'FGTS':                             '20095',
+    'IPE - INST PREV DO ESTADO - RS':   '30',
+    'MARINHA':                          '23',
+    'PREFEITURA DE PORTO ALEGRE - RS':  '10226',
+    'DÉBITO EM CONTA - RENEGOCIAÇÃO':   '710',
+    'FACTA FÁCIL - DÉBITO EM CONTA':    '390',
+    'PODER JUDICIARIO - RS':            '100'
+}
+
+
+def proposta_simulador(page: Page, data: Data):
+    page.evaluate('(PROPOSTA_SIMULADOR) => document.querySelector(PROPOSTA_SIMULADOR).click()', PROPOSTA_SIMULADOR)
+
+    # Dados da Proposta
+    sleep(5)
+    page.locator(DADOS_DA_PROPOSTA_PRODUTO).select_option(data['dados_da_proposta_produto'])
+    
+    sleep(1)
+    page.locator(DADOS_DA_PROPOSTA_TIPO_DE_OPERACAO).select_option(tipo_de_operacao[data['dados_da_proposta_tipo_de_operacao']])
+
+    sleep(1)
+    page.locator(DADOS_DA_PROPOSTA_ORGAO_OU_EMPREGADOR).select_option(orgao_ou_empregador[data['dados_da_proposta_orgao_ou_empregador']])
+
+    sleep(1)
+    page.locator(DADOS_DA_PROPOSTA_BANCO).select_option('3')
+
+    # Dados do Cliente
+    sleep(1)
+    page.locator(DADOS_DO_CLIENTE_CPF).type(data['dados_do_cliente_cpf'])
+    
+    sleep(1)
+    page.locator(DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO).type('')
+    
+    sleep(5)
+    page.locator(DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO).type(data['dados_do_cliente_data_de_nascimento'])
+    
+    sleep(1)
+    page.locator(DADOS_DO_CLIENTE_NOME_DO_CLIENTE).type(data['dados_do_cliente_nome_do_cliente'])
+
+    # Dados do Representante Legal
+
+    sleep(1)
+    page.locator(DADOS_DO_REPRESENTANTE_LEGAL_CPF).type(data['dados_do_representante_legal_cpf'])
+
+    sleep(1)
+    page.locator(DADOS_DO_REPRESENTANTE_LEGAL_NOME).type(data['dados_do_representante_legal_nome'])
