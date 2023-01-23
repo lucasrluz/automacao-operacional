a717780d90343ab31b47d63567743c3e8a1da46e
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-23 14:18:39 -0300

    correção nas variáveis de ambiente

--- automacao_operacional/simulador/steps/login.py
+++ automacao_operacional/simulador/steps/login.py
@@ -9,6 +9,6 @@ from .util.element_identifiers import (
 env = dotenv_values()
 
 def login(page: Page):
-    page.locator(LOGIN_USERNAME).type(env['USERNAME'])
-    page.locator(LOGIN_PASSWORD).type(env['PASSWORD'])
+    page.locator(LOGIN_USERNAME).type(env['SIMULADOR_USERNAME'])
+    page.locator(LOGIN_PASSWORD).type(env['SIMULADOR_PASSWORD'])
     page.locator(LOGIN_BUTTON_SUBMIT).click()
