e9306562ec1d89b98c2834601d9f0f280f55bd37
lucasrluz <lucasr.luzbr@gmail.com>
2023-01-30 09:32:23 -0300

    correção em seleção de valores de forma dinâmica

--- automacao_operacional/simulador/steps/first_page/simulador_dados_do_cliente.py
+++ automacao_operacional/simulador/steps/first_page/simulador_dados_do_cliente.py
@@ -4,18 +4,10 @@ from time import sleep
 from simulador.steps.util.element_identifiers import (
     DADOS_DO_CLIENTE_CPF,
     DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO,
-    DADOS_DO_CLIENTE_NOME_DO_CLIENTE
 )
 
 def simulador_dados_do_cliente(page: Page, data: Data):
     sleep(0.5)
-    page.locator(DADOS_DO_CLIENTE_CPF).type(data['dados_do_cliente_cpf'])
-    
-    sleep(0.5)
-    page.locator(DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO).type('')
-    
-    sleep(5)
-    page.locator(DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO).type(data['dados_do_cliente_data_de_nascimento'])
-    
-    sleep(0.5)
-    page.locator(DADOS_DO_CLIENTE_NOME_DO_CLIENTE).type(data['dados_do_cliente_nome_do_cliente'])
+    page.locator(DADOS_DO_CLIENTE_CPF).fill(data['dados_do_cliente_cpf'])
+
+    page.locator(DADOS_DO_CLIENTE_DATA_DE_NASCIMENTO).click()
