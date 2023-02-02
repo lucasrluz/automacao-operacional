2c0ad7ea42a1e3efbb784b269901c253612f2141
lucasrluz <lucasr.luzbr@gmail.com>
2023-02-02 11:51:57 -0300

    selecionando novos valores em informações de contato

--- automacao_operacional/simulador/steps/third_page/informacoes_de_contato.py
+++ automacao_operacional/simulador/steps/third_page/informacoes_de_contato.py
@@ -15,9 +15,14 @@ from simulador.steps.util.element_identifiers import (
     ESCOLARIDADE,
     BENEFICIO_OU_MATRICULA,
     CELULAR,
-    VALOR_BENEFICIO_OU_RENDA
+    VALOR_BENEFICIO_OU_RENDA,
+    NACIONALIDADE,
+    NATURALIDADE,
+    VALOR_PRATIMONIAL,
+    NATURALIDADE_UF
 )
 from simulador.steps.util.citys import citys
+from simulador.steps.util.city_uf import city_uf
 from simulador.steps.util.data import Data
 from time import sleep
 
@@ -26,46 +31,27 @@ def informacoes_de_contato(page: Page, data: Data):
 
     page.locator(ESTADO_CIVIL).select_option('6') # OUTROS
     page.locator(NOME_DO_PAI_NAO_CONSTA_NO_DOCUMENTO).check() # True
+    page.locator(VALOR_PRATIMONIAL).select_option('1') # Tento faz
     page.locator(CLIENTE_ILETRADO_OU_IMPOSSIBILITADO_DE_ASSINAR).select_option('N') # NÃO
     page.locator(ESCOLARIDADE).select_option('7')
 
-    # page.locator(BENEFICIO_OU_MATRICULA).fill(data['informacoes_do_beneficio_beneficio_ou_matricula'])
+    page.locator(NACIONALIDADE).select_option('1')
+    page.locator(NATURALIDADE_UF).select_option(data['informacoes_pessoais_naturalidade_uf'])
+    page.locator(NATURALIDADE).select_option(city_uf[data['informacoes_pessoais_naturalidade']])
+    
     valor_beneficio_ou_renda = data['informacoes_do_beneficio_valor_beneficio_ou_renda']
-    page.evaluate(f'(VALOR_BENEFICIO_OU_RENDA) => document.querySelector(VALOR_BENEFICIO_OU_RENDA).value = {valor_beneficio_ou_renda}', VALOR_BENEFICIO_OU_RENDA)
-
+    page.evaluate(f'(VALOR_BENEFICIO_OU_RENDA) => document.querySelector(VALOR_BENEFICIO_OU_RENDA).value = "{valor_beneficio_ou_renda}"', VALOR_BENEFICIO_OU_RENDA)
+    beneficio_ou_matricula = data['informacoes_do_beneficio_beneficio_ou_matricula']
+    page.evaluate(f'(BENEFICIO_OU_MATRICULA) => document.querySelector(BENEFICIO_OU_MATRICULA).value = "{beneficio_ou_matricula}"', BENEFICIO_OU_MATRICULA)
     page.locator(CEP_INFORMACOES_DE_CONTATO).fill(data['informacoes_de_contato_cep'])
     page.locator(BUTTON_PESQUISAR_CEP).click()
 
-    sleep(1)
-    error_cep_not_found: str = page.evaluate('(ERROR_CEP_NOT_FOUND) => document.querySelector(ERROR_CEP_NOT_FOUND)', ERROR_CEP_NOT_FOUND)
+    page.locator(ENDERECO_RESIDENCIAL).fill(data['informacoes_de_contato_endereco'])
+    page.locator(NUMERO).fill(data['informacoes_de_contato_numero'])
+    page.locator(COMPLEMENTO).fill(data['informacoes_de_contato_cemplemento'])
+    page.locator(BAIRRO).fill(data['informacoes_de_contato_bairro'])
+    page.locator(CIDADE).select_option(citys[data['informacoes_de_contato_cidade']])
+    page.locator(TIPO_RESIDENCIA).select_option('3') # ALUGADA
+    page.locator(CELULAR).fill(data['informacoes_de_contato_celular'])
 
-    if error_cep_not_found != None:
-        print('CEP não encontrado')
-        page.locator(ENDERECO_RESIDENCIAL).fill(data['informacoes_de_contato_endereco'])
-        page.locator(NUMERO).fill(data['informacoes_de_contato_numero'])
-        page.locator(COMPLEMENTO).fill(data['informacoes_de_contato_cemplemento'])
-        page.locator(BAIRRO).fill(data['informacoes_de_contato_bairro'])
-        page.locator(CIDADE).select_option(citys[data['informacoes_de_contato_cidade']])
-        page.locator(TIPO_RESIDENCIA).select_option('3') # ALUGADA
-        page.locator(CELULAR).fill(data['informacoes_de_contato_celular'])
-        sleep(3)
-        return
-    
-    # Get City, remove after
-    # i = 1
-    # with open('b.txt', 'w') as f:
-    #     while True:
-    #         print(i)
-    #         id = f'#cidade > option:nth-child({i})'
-    #         if page.evaluate('(id) => document.querySelector(id)', id) == None:
-    #             break
-    #         id = f'#cidade > option:nth-child({i})'
-
-    #         city = page.evaluate('(id) => document.querySelector(id).innerHTML', id)
-    #         value = page.evaluate('(id) => document.querySelector(id).value', id)
-
-    #         f.write(f"'{city}': '{value}',")
-    #         f.write('\n')
-
-    #         i += 1
-
+    return
